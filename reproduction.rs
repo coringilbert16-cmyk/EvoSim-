ded948159c4e2b5bda0be63a915308d4fd22611f
coringilbert16-cmyk <coringilbert16@gmail.com>
2026-09-03 17:55:25 -0400

    Remove unused reproduction import

--- reproduction.rs
+++ reproduction.rs
@@ -8,7 +8,6 @@
 //! only create a separate organism when the developing structure reaches the
 //! genetically determined juvenile threshold.
 
-use rand::SeedableRng;
 use rand_chacha::ChaCha8Rng;
 
 use crate::state::{DevelopmentStage, Organism, ReproductiveConstruction};
