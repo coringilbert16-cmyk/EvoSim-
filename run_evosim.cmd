e9a43a44c702c3efc49e8c196bb4f427357b00e2
coringilbert16-cmyk <coringilbert16@gmail.com>
2026-09-08 08:38:29 -0400

    Use default browser for EvoSim launcher

--- run_evosim.cmd
+++ run_evosim.cmd
@@ -15,15 +15,5 @@ pause
 exit /b 1
 
 :ready
-set "EDGE="
-if exist "%ProgramFiles(x86)%\Microsoft\Edge\Application\msedge.exe" set "EDGE=%ProgramFiles(x86)%\Microsoft\Edge\Application\msedge.exe"
-if not defined EDGE if exist "%ProgramFiles%\Microsoft\Edge\Application\msedge.exe" set "EDGE=%ProgramFiles%\Microsoft\Edge\Application\msedge.exe"
-if not defined EDGE if exist "%LocalAppData%\Microsoft\Edge\Application\msedge.exe" set "EDGE=%LocalAppData%\Microsoft\Edge\Application\msedge.exe"
-
-if defined EDGE (
-    start "EvoSim" "%EDGE%" --app=http://127.0.0.1:3000/ --start-maximized
-) else (
-    echo Microsoft Edge was not found; opening EvoSim in the default browser.
-    start "" http://127.0.0.1:3000/
-)
+start "" http://127.0.0.1:3000/
 endlocal
