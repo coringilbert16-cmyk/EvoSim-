3c55fba47d2d80e6a8b866c862e334a4b059c818
coringilbert16-cmyk <coringilbert16@gmail.com>
2026-09-14 12:55:25 -0400

    Remove unused construction context re-export

--- src/construction_realization.rs
+++ src/construction_realization.rs
@@ -1,4 +1,3 @@
 //! Unified construction facade.
 
 pub(crate) use crate::construction_legacy::{realize_material, realize_material_with_constraints};
-pub(crate) use crate::construction_runtime::realize_material_with_context;
