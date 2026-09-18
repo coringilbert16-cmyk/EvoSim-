fdb4c35b1db60095fdee4ae125cdbff858019ee2
coringilbert16-cmyk <coringilbert16@gmail.com>
2026-09-18 09:34:11 -0400

    Format occupied-cell acquisition assertion

--- src/simulation_tests.rs
+++ src/simulation_tests.rs
@@ -216,7 +216,10 @@ mod integration_tests {
         );
         s.step();
         assert_eq!(s.organisms[0].stored_material.total_amount(), 1.0);
-        assert_eq!(s.environment.field.cells[i].materials[0].total_amount(), 10.0);
+        assert_eq!(
+            s.environment.field.cells[i].materials[0].total_amount(),
+            10.0
+        );
     }
     #[test]
     fn structural_material_is_not_opened_by_storage() {
