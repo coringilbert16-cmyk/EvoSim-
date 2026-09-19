15a016b43c46fa95d012ea9659db7d48b292214b
coringilbert16-cmyk <coringilbert16@gmail.com>
2026-09-19 15:21:39 -0400

    Fix Clippy finding in src/cavity.rs

--- src/cavity.rs
+++ src/cavity.rs
@@ -387,7 +387,6 @@ mod tests {
     #[test]
     fn unbonded_structure_is_not_a_genome() {
         let catalog = default_catalog();
-        let genome = initial_genome();
         let blueprint = crate::juvenile::confirmed_seed_baseline(&default_catalog()).unwrap();
         let mut structure = blueprint.realize(&catalog).unwrap();
         structure.bonds.clear();
