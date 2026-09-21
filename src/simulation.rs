16842c991a2548ad51e6d68c453fc2116fc9b71c
coringilbert16-cmyk <coringilbert16@gmail.com>
2026-09-21 11:15:19 -0400

    Treat realized acquisition placement as world geometry

--- src/simulation.rs
+++ src/simulation.rs
@@ -204,8 +204,7 @@ impl Simulation {
                         .as_ref()
                         .and_then(|placements| placements.first())
                         .is_some_and(|placement| {
-                            let (cell_x, cell_y) = environment.field.cell_center(field_index);
-                            body.contains_point(cell_x + placement.x, cell_y + placement.y)
+                            body.contains_point(placement.x, placement.y)
                         })
             })
         });
