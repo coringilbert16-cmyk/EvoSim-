2686ffb2441f188fcaa2aaa90540591eb85242ee
coringilbert16-cmyk <coringilbert16@gmail.com>
2026-09-22 13:27:43 -0400

    Retire legacy perception module

--- src/main.rs
+++ src/main.rs
@@ -42,7 +42,6 @@ mod memory;
 mod movement;
 mod movement_direction;
 mod observation;
-mod perception;
 mod reproduction;
 #[path = "structural_blueprint_unified.rs"]
 #[allow(clippy::needless_range_loop, unused_variables)]
