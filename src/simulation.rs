4bbf513a9b04113ae1efcb068946d2671fdfc71d
coringilbert16-cmyk <coringilbert16@gmail.com>
2026-09-23 17:33:56 -0400

    Split recycling helper from simulation runtime

--- src/simulation.rs
+++ src/simulation.rs
@@ -254,42 +254,6 @@ impl Simulation {
             })
             .collect()
     }
-    fn recycle_dead_organism(
-        environment: &mut Environment,
-        organism: &mut Organism,
-        ledger: &mut EnergyLedger,
-    ) -> Option<crate::decomposition::DecomposingBody> {
-        let position = organism.occupied_cells.first().cloned()?;
-        for entry in organism.stored_material.drain_entries() {
-            match entry {
-                crate::material_storage::StoredMaterial::Logical(material) => {
-                    environment.field.deposit(position.x, position.y, material);
-                }
-                crate::material_storage::StoredMaterial::Physical(material) => {
-                    environment.field.deposit(position.x, position.y, material);
-                }
-            }
-        }
-        if let Some(mut construction) = organism.reproductive_construction.take() {
-            for entry in construction.committed_material.drain_entries() {
-                match entry {
-                    crate::material_storage::StoredMaterial::Logical(material) => {
-                        environment.field.deposit(position.x, position.y, material);
-                    }
-                    crate::material_storage::StoredMaterial::Physical(material) => {
-                        environment.field.deposit(position.x, position.y, material);
-                    }
-                }
-            }
-        }
-        let mut body =
-            crate::decomposition::DecomposingBody::new(organism.structure.clone(), 0.0, position)?;
-        let energy = organism.usable_energy;
-        if !ledger.transfer(&mut organism.usable_energy, &mut body.energy_budget, energy) {
-            return None;
-        }
-        Some(body)
-    }
     fn process_decomposing_bodies(&mut self) {
         let mut finished_indices = Vec::new();
         for index in 0..self.decomposing_bodies.len() {
@@ -689,7 +653,7 @@ impl Simulation {
                         survivors.push(child);
                     }
                 }
-                if let Some(body) = Self::recycle_dead_organism(
+                if let Some(body) = crate::recycling::recycle_dead_organism(
                     &mut self.environment,
                     &mut organism,
                     &mut self.energy_ledger,
