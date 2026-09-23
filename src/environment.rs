3c284d4b635149734685cc11e1af25cf7b1957b5
coringilbert16-cmyk <coringilbert16@gmail.com>
2026-09-23 11:36:29 -0400

    Correct rustfmt import ordering

--- src/environment.rs
+++ src/environment.rs
@@ -6,8 +6,8 @@
 
 use serde::{Deserialize, Serialize};
 
-use crate::physical_material::PhysicalMaterial;
 use crate::material_transfer::take_whole_unstructured;
+use crate::physical_material::PhysicalMaterial;
 use crate::resources::{merge_parts, Material};
 
 pub const DEFAULT_CELL_SIZE: f64 = 25.0;
