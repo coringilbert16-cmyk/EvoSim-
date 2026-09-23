d27fead91ddcb74f093b9d1cce6028bd67552d92
coringilbert16-cmyk <coringilbert16@gmail.com>
2026-09-23 09:08:10 -0400

    Fix rustfmt formatting

--- src/transformation.rs
+++ src/transformation.rs
@@ -382,7 +382,6 @@ mod tests {
             active_transformation_id: None,
             reproductive_construction: None,
             structure_revision: 0,
-        position_revision: 0,
             position_revision: 0,
             cached_cavity_revision: None,
             cached_cavity: None,
