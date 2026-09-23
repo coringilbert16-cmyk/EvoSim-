5b95d0d3a020e75973abb4eae244dd7bafc3a22e
coringilbert16-cmyk <coringilbert16@gmail.com>
2026-09-23 09:08:07 -0400

    Fix rustfmt formatting

--- src/simulation.rs
+++ src/simulation.rs
@@ -86,7 +86,6 @@ impl Simulation {
             active_transformation_id: None,
             reproductive_construction: None,
             structure_revision: 0,
-        position_revision: 0,
             position_revision: 0,
             cached_cavity_revision: None,
             cached_cavity: None,
