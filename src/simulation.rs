6396af18f50698a7527315c7a8b47c912eb007b6
coringilbert16-cmyk <coringilbert16@gmail.com>
2026-09-23 10:56:13 -0400

    Apply rustfmt-compatible formatting

--- src/simulation.rs
+++ src/simulation.rs
@@ -510,7 +510,8 @@ impl Simulation {
                         for other in after.iter() {
                             others.push((*other).clone());
                         }
-                        let moved = Self::update_movement(organism, environment, &mut others, &mut self.rng);
+                        let moved =
+                            Self::update_movement(organism, environment, &mut others, &mut self.rng);
                         if moved {
                             for (original, trial) in
                                 before.iter_mut().chain(after.iter_mut()).zip(others)
