30fd65b6647636b0f4f9d90a94d90bf8855c1159
coringilbert16-cmyk <coringilbert16@gmail.com>
2026-09-25 16:26:39 -0400

    Apply CI rustfmt corrections

--- src/simulation.rs
+++ src/simulation.rs
@@ -41,8 +41,7 @@ impl Simulation {
         let height = 1000.0;
         let mut field = ActiveMaterialField::new(width, height, DEFAULT_CELL_SIZE);
         crate::environmental_materials::seed_initial_landscape(&mut field, &catalog);
-        let resource_cloud =
-            crate::environmental_materials::ResourceCloud::initial(500.0, 500.0);
+        let resource_cloud = crate::environmental_materials::ResourceCloud::initial(500.0, 500.0);
         crate::environmental_materials::seed_resource_cloud(
             &mut field,
             &catalog,
