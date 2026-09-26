ace73bd8dd03879f4155cfa813bdc9b69af4e0ac
coringilbert16-cmyk <coringilbert16@gmail.com>
2026-09-26 12:26:25 -0400

    Remove unused logical transformation material

--- src/transformation.rs
+++ src/transformation.rs
@@ -200,7 +200,6 @@ impl Simulation {
             id: *next_id,
             organism_id: organism.id.clone(),
             kind: crate::state::TransformationKind::Break,
-            material: crate::resources::Material::free_base("", 0.0),
             bond: None,
             stored_material,
             stored_bond,
